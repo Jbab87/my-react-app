473c972109cbbfe3cc59d8c87323b7686cbcf5f0
Jbab87 <jbabajide87@gmail.com>
2023-12-20 11:43:24 +0000

    separation into different component files

---
+++ src/Footer.js
@@ -0,0 +1,5 @@
+ export default function Footer() {
+    return (
+        <footer className="footer"> © 2023 J development. All rights reserved. </footer>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import navlogo from "./logo192.png"
+
+export default function Header() {
+    return (
+    <header>
+        <nav className="nav">
+        <img className="nav-logo" src={navlogo}></img>
+        <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        </nav>
+    </header>
+    )
+}
+
+ 
